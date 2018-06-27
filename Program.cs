5f5e2ee305474dd4b592e33544156c302fe7d3b2
Nick Strupat <Nick@Nicks-MacBook-Pro.local>
2018-06-27 00:10:43 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Antlr4.Runtime;
+
+namespace Cova
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            var input = 
+@"begin
+    let a be 5
+    let b be 10
+    add 3 to b
+    add b to a
+    add a to b
+    print b
+    print 3
+end";
+
+            var stream = CharStreams.fromstring(input);
+			var lexer = new GyooLexer(stream);
+			var tokens = new CommonTokenStream(lexer);
+			var parser = new GyooParser(tokens);
+			var what = new GyooVisitor();
+            what.Visit(parser.program());
+        }
+    }
+
+    class GyooVisitor : GyooBaseVisitor<Int32> {
+        public override Int32 VisitAssign(GyooParser.AssignContext context) {
+            return 42;
+        }
+    }
+}
