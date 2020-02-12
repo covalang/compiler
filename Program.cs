3531d6d64e8cad2efa5105e128c19be60d6f5e99
Nicholas Strupat <nick@Nick-MBP-MacOS.local>
2020-02-12 17:25:33 -0500

    more

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.IO.MemoryMappedFiles;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using Compiler;
 
 namespace Cova
@@ -121,9 +122,178 @@ namespace Cova
 		{
 		}
 
+		enum UnicodeCharacterClass : byte
+		{
+			Cn,
+			Cc,
+			Cf,
+			Co,
+			Cs,
+			Ll,
+			Lm,
+			Lo,
+			Lt,
+			Lu,
+			Mc,
+			Me,
+			Mn,
+			Nd,
+			Nl,
+			No,
+			Pc,
+			Pd,
+			Pe,
+			Pf,
+			Pi,
+			Po,
+			Ps,
+			Sc,
+			Sk,
+			Sm,
+			So,
+			Zl,
+			Zp,
+			Zs
+		}
+
+		enum UnicodeCharacterClassName : byte
+		{
+			OtherNotAssigned,
+			Control,
+			Format,
+			PrivateUse,
+			Surrogate,
+			LowercaseLetter,
+			ModifierLetter,
+			OtherLetter,
+			TitlecaseLetter,
+			UppercaseLetter,
+			SpacingCombiningMark,
+			EnclosingMark,
+			NonSpacingMark,
+			DecimalDigitNumber,
+			LetterNumber,
+			OtherNumber,
+			ConnectorPunctuation,
+			DashPunctuation,
+			ClosePunctuation,
+			FinalQuotePunctuation,
+			InitialQuotePunctuation,
+			OtherPunctuation,
+			OpenPunctuation,
+			CurrencySymbol,
+			ModifierSymbol,
+			MathSymbol,
+			OtherSymbol,
+			LineSeparator,
+			ParagraphSeparator,
+			SpaceSeparator,
+		}
+
+		struct Range {
+			public int First;
+			public int Last;
+			public int Size => Last - First + 1;
+			
+			public override String ToString() => First == Last ? First.ToString() : $"{First}-{Last}: {Size}";
+		}
+
+		enum State
+		{
+			StartMaybeRange,
+			ExplicitRange
+		}
+
+		// static List<List<UInt32>> GetFactors(UInt32 x) {
+		// 	var factors = new List<List<UInt32>>();
+		// 	var hold = new List<UInt32>();
+		// 	for (var i = 2; i < x; i++) {
+		// 		if (hold.Aggregate((y, z) => y *= z) == x) {
+
+		// 		}
+		// 	}
+		// }
+
         static void Main(String[] args)
         {
-	        var lines = File.ReadAllLines(@"C:\Users\nicks\Downloads\UnicodeData.txt");
+	        //var lines = File.ReadAllLines(@"C:\Users\nicks\Downloads\UnicodeData.txt");
+	        var lines = File.ReadAllLines(@"/Users/Nick/Downloads/UCD/UnicodeData.txt");
+			var classRanges = new Dictionary<UnicodeCharacterClass, List<Range>>();
+			var unicodeCharacterClassNames = Enum.GetNames(typeof(UnicodeCharacterClass));
+			foreach (UnicodeCharacterClass x in Enum.GetValues(typeof(UnicodeCharacterClass)))
+				classRanges[x] = new List<Range>();
+			
+			var range = default(Range);
+			var hold = default(UnicodeCharacterClass);
+			var previousCodePoint = -1;
+			var state = 0;
+			foreach (var line in lines)
+			{
+				var parts = line.Split(';');
+				var codePoint = Int32.Parse(parts[0], NumberStyles.HexNumber);
+				var ucc = (UnicodeCharacterClass) Enum.Parse(typeof(UnicodeCharacterClass), parts[2]);
+				var isFirst = parts[1].EndsWith("First>");
+				var isLast = parts[1].EndsWith("Last>");
+				switch (state)
+				{
+					case 0:
+						range.First = codePoint;
+						hold = ucc;
+						state = isFirst ? 1 : 2;
+						break;
+					case 1:
+						if (hold != ucc)
+							throw new Exception("First-Last pair with mismatched character class");
+						range.Last = codePoint;
+						classRanges[ucc].Add(range);
+						range = default;
+						state = 0;
+						break;
+					case 2:
+						if (hold == ucc)
+							break;
+						range.Last = previousCodePoint;
+						classRanges[hold].Add(range);
+						range = default;
+						hold = ucc;
+						state = 0;
+						goto case 0;
+				}
+				previousCodePoint = codePoint;
+			}
+			var orderByDescMax = classRanges.OrderByDescending(x => x.Value.Any() ? x.Value.Max(y => y.Size) : 0).ToList();
+			var orderByDescSum = classRanges.OrderByDescending(x => x.Value.Any() ? x.Value.Sum(y => y.Size) : 0).ToList();
+			var max = classRanges.Max(x => x.Value.Any() ? x.Value.Max(y => y.Last) : 0);
+			var bits = Math.Ceiling(Math.Log(max, 2));
+			var tableSize = (Int32)Math.Pow(2, bits);
+			var table = new Byte[tableSize];
+			foreach (var classRange in classRanges)
+				foreach (var x in classRange.Value)
+					for (var i = x.First; i <= x.Last; i++)
+						table[i] = (Byte) classRange.Key;
+			//File.WriteAllBytes("UnicodeCharacterClassesTable.data", table);
+			
+			// calculate list of non-zero numbers which sum to 21
+			var levelSizes = (Int32) bits / 3;
+			var levelMask = (Int32) Math.Pow(2, levelSizes) - 1;
+			// var level1 = new List<Int32>();
+			// var level2 = new List<Int32>();
+			// var level3 = new List<Int32>();
+			// for (var i = 0; i != table) {
+			// 	var a = x >> (levelSizes * 2);
+			// 	var b = (x >> levelSizes) & levelMask;
+			// 	var c = x  & levelMask;
+			// }
+			var level1 = table.Select((x,i) => (i >> (levelSizes * 2), x)).Distinct().GroupBy(x => x.Item1, x => x.).ToList();
+			var level2 = table.Select((x,i) => ((i >> levelSizes) & levelMask, x)).Distinct().ToList();
+			var level3 = table.Select((x,i) => (i & levelMask, x)).Distinct().ToList();
+			unsafe {
+				void * p = Marshal.AllocHGlobal(32).ToPointer();
+				Marshal.FreeHGlobal(new IntPtr(p));
+			}
+			var final = (UnicodeCharacterClass[]) Enum.GetValues(typeof(UnicodeCharacterClass));
+			return;
+
 	        var asdf = new (UInt32, UnicodeCategory)[0x10ffff];
 	        for (UInt32 i = 0; i < lines.Length; i++)
 	        {
