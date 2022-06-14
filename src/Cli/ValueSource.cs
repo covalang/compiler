32a36d80b47a3dae7fe34fb0d0c6392694f208a0
NickStrupat <nick.strupat@gmail.com>
2022-06-14 14:24:51 -0400

    Fix nullability of generic parameter.

--- src/Cli/ValueSource.cs
+++ src/Cli/ValueSource.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Cova.Cli;
 
-sealed class ValueSource<T> : BinderBase<T>
+sealed class ValueSource<T> : BinderBase<T> where T : notnull
 {
 	protected override T GetBoundValue(BindingContext bindingContext) => bindingContext.GetRequiredService<T>();
 }
