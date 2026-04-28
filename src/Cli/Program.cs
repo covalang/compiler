64b0ea31714542921da402978ec01a3d1839df62
Nick Strupat <nick.strupat@gmail.com>
2026-04-27 23:46:54 -0400

    cli

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,64 +1,267 @@
-﻿using System.CommandLine;
-using System.CommandLine.Builder;
-using System.CommandLine.Parsing;
-using System.IO.Pipes;
-using System.Net;
-using System.Net.Sockets;
-using Cova.Cli;
-using Cova.LanguageServer;
+﻿using System.Reflection;
+using ConsoleAppFramework;
+using Microsoft.Extensions.DependencyInjection;
+using NoAlloq;
+// using static CmdLn;
 
-var root = new RootCommand("Cova programming language");
-var ctvs = new ValueSource<CancellationToken>();
-var @new = new Command("new", "Create a new project");
-var run = new Command("run", "Run a project");
-var build = new Command("build", "Build a project");
-var clean = new Command("clean", "Clean a project");
-var test = new Command("test", "Run tests");
-var serve = new Command("serve", "Run language server");
-var restore = new Command("restore", "Restore packages");
-foreach (var command in new[] { @new, run, build, clean, test, serve, restore })
-	root.AddCommand(command);
+var sc = new ServiceCollection();
 
-var stdIo = new Command("stdio", "Use stdin/stdout for LSP communication");
-var pipe = new Command("pipe", "Use a named pipe for LSP communication");
-var socket = new Command("socket", "Use a TCP port to connect for LSP communication");
-foreach (var command in new[] { stdIo, pipe, socket })
-	serve.AddCommand(command);
-
-stdIo.SetHandler(HandleStdIo, ctvs);
-
-var pipeName = new Argument<String>("name", "Name of the pipe");
-pipe.AddArgument(pipeName);
-pipe.SetHandler(HandleNamedPipe, pipeName, ctvs);
-
-var portNumber = new Argument<UInt16>("port", "TCP port number");
-socket.AddArgument(portNumber);
-socket.SetHandler(HandleSocket, portNumber, ctvs);
-
-return await new CommandLineBuilder(root).UseDefaults().Build().InvokeAsync(args);
-
-
-static async Task<Int32> HandleStdIo(CancellationToken cancellationToken)
+//ConsoleApp.ServiceProvider = sc.BuildServiceProvider();
+var cab = ConsoleApp.Create();
+cab.ConfigureServices((cac, services) =>
 {
-	var input = Console.OpenStandardInput();
-	var output = Console.OpenStandardOutput();
-	return await new Server(input, output).RunAsync(cancellationToken);
+	services.AddSingleton("World");
+});
+cab.Add<Root>();
+cab.Add<Root.Package>("package");
+await cab.RunAsync(args);
+
+public sealed class Root(String s)
+{
+	/// <summary>Run a project.</summary>
+	public async Task Run(CancellationToken ct)
+	{
+		Console.WriteLine($"Hello, {s}!");
+	}
+
+	/// <summary>Build a project.</summary>
+	public void Build() {}
+
+	/// <summary>Clean a project.</summary>
+	public void Clean() {}
+
+	/// <summary>Run tests.</summary>
+	public void Test() {}
+
+	/// <summary>Run language server.</summary>
+	public void Serve() {}
+
+	public sealed class Package
+	{
+		/// <summary>Install a package.</summary>
+		public void Install() {}
+
+		/// <summary>Uninstall a package.</summary>
+		public void Uninstall() {}
+
+		/// <summary>Update a package.</summary>
+		public void Update() {}
+
+		/// <summary>Restore packages.</summary>
+		public void Restore() {}
+	}
 }
 
-static async Task<Int32> HandleNamedPipe(String pipeName, CancellationToken cancellationToken)
-{
-	const String pipePrefix = @"\\.\pipe\";
-	if (pipeName.StartsWith(pipePrefix))
-		pipeName = pipeName[pipePrefix.Length..]; // VSCode on Windows prefixes the pipe with \\.\pipe\
-	var pipe = new NamedPipeClientStream(".", pipeName, PipeDirection.InOut);
-	pipe.Connect();
-	return await new Server(pipe, pipe, pipe).RunAsync(cancellationToken);
-}
-
-static async Task<Int32> HandleSocket(UInt16 portNumber, CancellationToken cancellationToken)
-{
-	var tcp = new TcpClient();
-	tcp.Connect(IPAddress.Loopback, portNumber);
-	var stream = tcp.GetStream();
-	return await new Server(stream, stream, tcp).RunAsync(cancellationToken);
-}
+// CmdLn.Run(args, [
+// 	Cmd("run", 'r', "Run a project", () => Console.WriteLine("Run")),
+// 	Cmd("build", 'b', "Build a project", () => Console.WriteLine("Build")),
+// 	Cmd("clean", 'c', "Clean a project", () => Console.WriteLine("Clean")),
+// 	Cmd("test", 't', "Run tests", () => Console.WriteLine("Test")),
+// 	Cmd("serve", 's', "Run language server", [
+// 		SubCmd("stdio", 's', "Use stdin/stdout for LSP communication", () => Console.WriteLine("Serve")),
+// 		SubCmd("pipe", 'p', "Use a named pipe for LSP communication", [
+// 			Param<String>("name", 'n', "Name of the pipe", () => Console.WriteLine("Serve"))
+// 		]),
+// 		SubCmd("socket", 's', "Use a TCP port to connect for LSP communication", [
+// 			Param<Int32>("port", 'p', "TCP port number", () => Console.WriteLine("Serve"))
+// 		])
+// 	]),
+// 	new("restore", 'r', "Restore packages", () => Console.WriteLine("Restore"))
+// ]);
+//
+// // Commands commands = new()
+// // {
+// // 	["run", 'r', "Run a project"] = () => {},
+// // 	["build", 'b', "Build a project"] = () => {},
+// // 	["clean", 'c', "Clean a project"] = () => {},
+// // 	["test", 't', "Run tests"] = () => {},
+// // 	["serve", 's', "Run language server"] = new()
+// // 	{
+// // 		["stdio", 's', "Use stdin/stdout for LSP communication"] = () => {},
+// // 		["pipe", 'p', "Use a named pipe for LSP communication"] = new()
+// // 		{
+// // 			["name"] = () => {}
+// // 		},
+// // 		["socket", 's', "Use a TCP port to connect for LSP communication"] = new()
+// // 		{
+// // 			["port"] = () => {}
+// // 		}
+// // 	},
+// // 	["restore", 'r', "Restore packages"] = () => {}
+// // };
+//
+// // Dictionary<Foo, Bar> handlers = new()
+// // {
+// // 	["run"] = new(),
+// // 	["build"] = new(),
+// // 	["clean"] = new(),
+// // 	["test"] = new(),
+// // 	["serve"] = new(),
+// // 	["restore"] = new(),
+// // 	["serve stdio"] = new(),
+// // 	["serve pipe"] = new(),
+// // 	["serve socket"] = new(),
+// // };
+// //
+// // 	() => {},
+// // 	(String name, Int32 count) => {},
+// // ];
+//
+// // Command[] commands = [
+// // 	new('n', "new", "Create a new project"),
+// // 	new('r', "run", "Run a project"),
+// // 	new('b', "build", "Build a project"),
+// // 	new('c', "clean", "Clean a project"),
+// // 	new('t', "test", "Run tests"),
+// // 	new('s', "serve", "Run language server", SubCommands: [
+// // 		new('s', "stdio", "Use stdin/stdout for LSP communication"),
+// // 		new('p', "pipe", "Use a named pipe for LSP communication", Arguments: [
+// // 			new Argument<String>("name", "Name of the pipe")
+// // 		]),
+// // 		new('s', "socket", "Use a TCP port to connect for LSP communication", Arguments: [
+// // 			new Argument<UInt16>("port", "TCP port number")
+// // 		])
+// // 	]),
+// // 	new('r', "restore", "Restore packages")
+// // ];
+//
+// // var command = commands.Where(x => x.ShortName == args[0]?[0] || x.LongName == args[0]);
+//
+// interface ICliParser
+// {
+// 	static abstract ReadOnlySpan<Command> Commands { get; }
+// 	static abstract
+// }
+//
+// readonly struct Commands
+// {
+// 	private readonly Dictionary<String, (String description, Delegate command)> longNameCommandMap = new();
+// 	private readonly Dictionary<Char, (String description, Delegate command)> shortNameCommandMap = new();
+//
+// 	public Commands() {}
+//
+// 	public Delegate this[String longName, String description]
+// 	{
+// 		set => longNameCommandMap[longName] = (description, value);
+// 	}
+//
+// 	public Delegate this[String longName, Char shortName, String description]
+// 	{
+// 		set
+// 		{
+// 			longNameCommandMap[longName] = (description, value);
+// 			shortNameCommandMap[shortName] = (description, value);
+// 		}
+// 	}
+// }
+//
+// struct Arguments
+// {
+// 	private Dictionary<String, Argument> longNameArgumentMap = new();
+// 	private Dictionary<Char, Argument> shortNameArgumentMap = new();
+//
+// 	public Arguments() {}
+//
+// 	public Argument this[String longName] { set => longNameArgumentMap[longName] = value; }
+//
+// 	public Argument this[String longName, Char shortName]
+// 	{
+// 		set
+// 		{
+// 			longNameArgumentMap[longName] = value;
+// 			shortNameArgumentMap[shortName] = value;
+// 		}
+// 	}
+// }
+//
+// struct Foo
+// {
+// 	private Foo(String longName, Char? shortName) => (LongName, ShortName) = (longName, shortName);
+// 	public Char? ShortName { get; set; }
+// 	public String LongName { get; set; }
+//
+// 	public static implicit operator Foo(String longName) => new(longName, null);
+// 	public static implicit operator Foo((String longName, Char shortName) names) => new(names.longName, names.shortName);
+// }
+//
+// sealed record Command(
+// 	String LongName,
+// 	Char? ShortName,
+// 	String Description,
+// 	Commands? SubCommands = null,
+// 	Arguments? Arguments = null
+// ) : ICommand;
+//
+// sealed record Argument<T>(String Name, String Description) : Argument2(Name, Description);
+// abstract record Argument2(String Name, String Description);
+//
+// interface ICommand
+// {
+// 	public String LongName { get; }
+// 	public Char? ShortName { get; }
+// 	public String Description { get; }
+// 	public Commands? SubCommands { get; }
+// 	public Arguments? Arguments { get; }
+// }
+//
+// static class CmdLn
+// {
+// 	public static Int32 Run(String[] args, ReadOnlySpan<Cmd> commands)
+// 	{
+// 		if (args.Length == 0)
+// 		{
+// 			Console.WriteLine("No command specified");
+// 			return 1;
+// 		}
+//
+// 		var command = commands.FirstOrDefault(x => x.ShortName == args[0]?[0] || x.LongName == args[0]);
+// 		if (command is null)
+// 		{
+// 			Console.WriteLine("Command not found");
+// 			return 1;
+// 		}
+//
+// 		return 0;
+// 	}
+//
+// 	public static Cmd Cmd(String longName, Char? shortName, String description, Delegate command) =>
+// 		new(longName, shortName, description, command);
+//
+// 	public static Cmd Cmd(String longName, Char? shortName, String description, ReadOnlySpan<Cmd> subCommands) =>
+// 		new(longName, shortName, description, subCommands);
+//
+// 	public static Cmd SubCmd(String longName, Char? shortName, String description, Delegate command) =>
+// 		new(longName, shortName, description, command);
+//
+// 	public static Cmd SubCmd(String longName, Char? shortName, String description, ReadOnlySpan<Cmd> subCommands) =>
+// 		new(longName, shortName, description, subCommands);
+//
+// 	public static Cmd Param<T>(String name, Char? shortName, String description, Delegate command) =>
+// 		new(name, shortName, description, command);
+//
+// 	public static Cmd Param<T>(String name, Char? shortName, String description) =>
+// 		new(name, shortName, description);
+// }
+//
+// readonly struct Cmd
+// {
+// 	private Cmd(String longName, Char? shortName, String description)
+// 	{
+// 		LongName = longName;
+// 		ShortName = shortName;
+// 		Description = description;
+// 	}
+//
+// 	public Cmd(String longName, Char? shortName, String description, Delegate command)
+// 		: this(longName, shortName, description) => Command = command;
+//
+// 	public Cmd(String longName, Char? shortName, String description, ReadOnlySpan<Cmd> subCommands)
+// 		: this(longName, shortName, description) => SubCommands = subCommands;
+//
+// 	public String LongName { get; }
+// 	public Char? ShortName { get; }
+// 	public String Description { get; }
+// 	public Delegate? Command { get; }
+// 	public ReadOnlySpan<Cmd> SubCommands { get; }
+// }
+//
